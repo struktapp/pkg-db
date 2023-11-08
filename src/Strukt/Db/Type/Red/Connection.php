dbbf49b032f843e52d3b066969b6dbe5e32597f7
pitsolu <pitsolu@gmail.com>
2023-11-09 01:22:43 +0300

    check if rb_model_prefix is defined

--- src/Strukt/Db/Type/Red/Connection.php
+++ src/Strukt/Db/Type/Red/Connection.php
@@ -9,7 +9,7 @@ class Connection{
 	public function __construct(){
 
 		$app_name = config("app.name");
-		if(!is_null($app_name))
+		if(!is_null($app_name) && !defined("REDBEAN_MODEL_PREFIX"))
 			define('REDBEAN_MODEL_PREFIX', sprintf('\\%s\\', $app_name));
 
 		$dsn = sprintf('%s:host=%s;dbname=%s', config("db.type"), config("db.host"), config("db.name"));
