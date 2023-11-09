11f1fcd60aa771446bea2c58b9f660665c5f9eaf
pitsolu <pitsolu@gmail.com>
2023-11-09 22:05:54 +0300

    added facet provider for db

---
+++ package/lib/App/Provider/Facet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Provider;
+
+use Strukt\Ref;
+use Strukt\Contract\ProviderInterface;
+
+/**
+* @Name(facet)
+*/
+class Facet implements ProviderInterface{
+
+	use \Strukt\Traits\FacetHelper;
+
+	public function __construct(){
+
+		//
+	}
+
+	public function register(){
+
+		$self = $this;
+		event("provider.core", function(string $alias_ns, array $args = null) use($self){
+
+			if(!reg()->exists("nr"))
+				raise("[nr|Name Registry] does not exists!");
+
+			if(!$this->isQualifiedAlias($alias_ns)){
+
+				$model = db(str($alias_ns)->toSnake()->toLower()->yield());
+				$model->__construct(...$args);
+				
+				return $model;
+			}
+
+			if(!is_null($args))
+				return Ref::create($self->getNamespace($alias_ns))
+							->makeArgs($args)
+							->getInstance();
+
+			return Ref::create($self->getNamespace($alias_ns))
+					->noMake()
+					->getInstance();
+		});
+	}
+}
+
