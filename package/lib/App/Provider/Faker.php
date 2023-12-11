65e7d1b231b29ffa7af7a96b25cd12ff056e8a86
pitsolu <pitsolu@gmail.com>
2023-12-11 18:04:02 +0300

    removed fzaninotto/faker and fakerphp

--- package/lib/App/Provider/Faker.php
+++ package/lib/App/Provider/Faker.php
@@ -14,13 +14,13 @@ class Faker implements ProviderInterface{
 
 	public function __construct(){
 		
-		$this->faker = new \Faker\Generator();
-		$this->faker->addProvider(new \Faker\Provider\en_US\Person($this->faker));
-		$this->faker->addProvider(new \Faker\Provider\en_US\Address($this->faker));
-		$this->faker->addProvider(new \Faker\Provider\en_US\PhoneNumber($this->faker));
-		$this->faker->addProvider(new \Faker\Provider\en_US\Company($this->faker));
-		$this->faker->addProvider(new \Faker\Provider\Lorem($this->faker));
-		$this->faker->addProvider(new \Faker\Provider\Internet($this->faker));
+		$this->faker = new Faker\Generator();
+		$this->faker->addProvider(new Faker\Provider\en_US\Person($this->faker));
+		$this->faker->addProvider(new Faker\Provider\en_US\Address($this->faker));
+		$this->faker->addProvider(new Faker\Provider\en_US\PhoneNumber($this->faker));
+		$this->faker->addProvider(new Faker\Provider\en_US\Company($this->faker));
+		$this->faker->addProvider(new Faker\Provider\Lorem($this->faker));
+		$this->faker->addProvider(new Faker\Provider\Internet($this->faker));
 	}
 
 	public function register(){
